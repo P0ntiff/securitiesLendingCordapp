21cdb078b859462406eae8a33a6e54fe7152be7b
P0ntiff <gattasb@gmail.com>
2017-07-13 17:48:43 +1000

    Created new flow, LoanTerminationFlow, to model the exit of a securities loan from the ledger (and redelivery of assets to original owners)

---
+++ kotlin-source/src/main/kotlin/com/secLendModel/flow/securitiesLending/LoanTerminationFlow.kt
@@ -0,0 +1,39 @@
+package com.secLendModel.flow.securitiesLending
+
+import co.paralleluniverse.fibers.Suspendable
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.flows.FlowLogic
+import net.corda.core.flows.InitiatingFlow
+import net.corda.core.flows.StartableByRPC
+import net.corda.core.identity.Party
+
+/**
+ * Flow to model the conclusion of a security loan, creating a TXN with the following structure:
+ *
+ *  Commands: Exit/Terminate
+ *  Inputs:  -Cash Collateral + Margin (from lender),
+ *           -Securities (from borrower)
+ *           -SecurityLoan (owned by lender)
+ *  Outputs: -Cash (owned by borrower)
+ *           -Securities (owned by lender)
+ */
+ 
+object LoanTerminationFlow {
+    @StartableByRPC
+    @InitiatingFlow
+    open class Borrower(val loan : UniqueIdentifier) : FlowLogic<Unit>() {
+        @Suspendable
+        override fun call(): Unit {
+            //TODO: Implement, for now, the opposite logic of the LoanIssuanceFlow
+            return Unit
+        }
+    }
+
+    class Lender(val borrower : Party) : FlowLogic<Unit>() {
+        @Suspendable
+        override fun call(): Unit {
+
+            return Unit
+        }
+    }
+}
